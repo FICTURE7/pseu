dc96187f48506ee011f5c88ed2e24852cc09800b
FICTURE7 <ficture7@gmail.com>
2018-05-21 17:49:24 +0400

    add: slighly more appropriate lexer tests

--- test/main.c
+++ test/main.c
@@ -1,48 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "test.h"
 #include "lexer.h"
 #include "token.h"
 
-int test_lexer_invalid_hex() {
+struct test_data_token {
+	enum token_type type;
+	char *value;
+};
+
+struct test_data_lexer {
+	char *src;
+	size_t tokens_count;
+	struct test_data_token tokens[16];
+};
+
+int test_lexer() {
 	struct lexer lexer;
 	struct token token;
 
-	lexer_init(&lexer, NULL, "0x");
-	lexer_scan(&lexer, &token);
+	struct test_data_lexer data[] = {
+		{
+			.src = "xD",
+			.tokens = {
+				{ TOK_IDENT, "xD" }
+			},
+			.tokens_count = 1
+		},
+		{
+			.src = " 0  0.  0x  000  0e24  0.10    0.e5  0xFF   ",
+			.tokens = {
+				{ TOK_LIT_INTEGER, "0" },
+				{ TOK_LIT_REAL, "0." },
+				{ TOK_ERR, "0x" },
+				{ TOK_LIT_INTEGER, "000" },
+				{ TOK_LIT_REAL, "0e24" },
+				{ TOK_LIT_REAL, "0.10" },
+				{ TOK_LIT_REAL, "0.e5" },
+				{ TOK_LIT_INTEGERHEX, "0xFF" },
+			},
+			.tokens_count = 6
+		},
+		{
+			.src = " 1  5454  55.676  ",
+			.tokens = {
+				{ TOK_LIT_INTEGER, "1" },
+				{ TOK_LIT_INTEGER, "5454" },
+				{ TOK_LIT_REAL, "55.676" }
+			},
+			.tokens_count = 2
+		},
+		{
+			.src = "  :   ",
+			.tokens = {
+				{ TOK_COLON, ":" }
+			},
+			.tokens_count = 1
+		}
+	};
 
-	TEST_EQ(token.type, TOK_ERR);
+	for (int i = 0; i < sizeof(data) / sizeof(struct test_data_lexer); i++) {
+		struct test_data_lexer cur_data = data[i];
+		lexer_init(&lexer, NULL, cur_data.src);
+
+		for (int k = 0; k < cur_data.tokens_count; k++) {
+			struct test_data_token tdata = cur_data.tokens[k];
+			lexer_scan(&lexer, &token);
+			TEST_EQ_TOK(token, tdata.type, tdata.value);
+		}
+	}
 	return 0;
 }
 
-int test_lexer_single_digit_0() {
-	struct lexer lexer;
-	struct token token;
-
-	lexer_init(&lexer, NULL, "0");
-	lexer_scan(&lexer, &token);
-
-	TEST_EQ(token.type, TOK_LIT_INTEGER);
-	return 0;
-}
-
-int test_lexer_normal_number() {
-	struct lexer lexer;
-	struct token token;
-
-	lexer_init(&lexer, NULL, "0050");
-	lexer_scan(&lexer, &token);
-
-	TEST_EQ(token.type, TOK_LIT_INTEGER);
-	return 0;
-}
-
-int main(int argc, char **argv) {	
+int main(int argc, char **argv) {
 	TEST_INIT();
-	TEST(test_lexer_invalid_hex);
-	TEST(test_lexer_single_digit_0);
-	TEST(test_lexer_normal_number);
+	TEST(test_lexer);
 	TEST_DEINIT();
 
 	getchar();
-    return 0;
+	return 0;
 }
--- test/test.h
+++ test/test.h
@@ -3,24 +3,29 @@
 
 #include <string.h>
 
-#define TEST_INIT()							\
+#define TEST_INIT()									\
 	printf(">> running tests\n------------\n")
 
-#define TEST_DEINIT()						\
+#define TEST_DEINIT()								\
 	printf("------------\n>> done\n")
 
-#define TEST(x)								\
-	printf(">>> running: %s", #x);			\
-	if (x()) {								\
-		printf(" -> failed\n");				\
-	} else {								\
-		printf(" -> success\n");			\
+#define TEST(x)										\
+	printf(">>> running: %s", #x);					\
+	if (x()) {										\
+		printf(" -> failed\n");						\
+	} else {										\
+		printf(" -> success\n");					\
 	}
 
-#define TEST_ASSERT(x)						\
+#define TEST_ASSERT(x)								\
 	if (x != 1) return 1;
 
-#define TEST_EQ(x, y) \
+#define TEST_EQ(x, y)								\
 	TEST_ASSERT(x == y)
+
+#define TEST_EQ_TOK(t, tt, tv)						\
+	TEST_ASSERT(t.type == tt);						\
+	TEST_ASSERT(t.len == strlen(tv));						\
+	TEST_ASSERT(memcmp(tv, t.loc.pos, strlen(tv)) == 0)	\
 
 #endif
