1f764dc9c1ce8e5d6d73b64e6a63cd85d25cc942
FICTURE7 <ficture7@gmail.com>
2018-07-19 22:26:10 +0400

    diagnostic: add comments

--- lib/diagnostic.h
+++ lib/diagnostic.h
@@ -1,18 +1,23 @@
 #ifndef DIAGNOSTIC_H
 #define DIAGNOSTIC_H
 
-#include "vector.h"
 #include "location.h"
 
+/* type of diagnostic */
 enum diagnostic_type {
-	DIAGNOSTIC_TYPE_ERROR,
-	DIAGNOSTIC_TYPE_WARNING,
+	DIAGNOSTIC_TYPE_ERROR, /* error diagnostic */
+	DIAGNOSTIC_TYPE_WARNING, /* warning diagnostic */
 };
 
+/*
+ * represents a diagnostic
+ * which may be about a lexical error,
+ * runtime error etc
+ */
 struct diagnostic {
-	enum diagnostic_type type;
-	struct location loc;
-	char *message;
+	enum diagnostic_type type; /* type of diagnostic */
+	struct location loc; /* location in source of the error */
+	char *message; /* null terminated string message descripting the diagnostic */
 };
 
 #endif
