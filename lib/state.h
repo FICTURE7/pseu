b2218228cd3a1ef20701e7746e14e477365f09ba
FICTURE7 <ficture7@gmail.com>
2018-07-19 22:16:54 +0400

    state: add comments

--- lib/state.h
+++ lib/state.h
@@ -4,13 +4,15 @@
 #include "string.h"
 #include "vector.h"
 
+/* represents a state */
 struct state {
-	struct vector *diagnostics;
-	struct string_table *strings;
+	struct vector *diagnostics; /* list of diagnostics related to the state. */
+	struct string_table *strings; /* string table in the state. */
+
+	void (*onerror)(int errcode); /* function to call whenever stuff happens. */
 };
 
 void state_init(struct state *state);
-
 void state_deinit(struct state *state);
 
 #endif /* STATE_H */
