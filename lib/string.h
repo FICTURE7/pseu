a40adf1064fded3d189b23a58aee3dbeb03ea5d5
FICTURE7 <ficture7@gmail.com>
2018-07-24 12:36:56 +0400

    string: add comments

--- lib/string.h
+++ lib/string.h
@@ -11,8 +11,7 @@
  */
 struct string_table_entry {
 	struct string_object *item;
-	/* next item in the link list/chain */
-	struct string_table_entry *next;
+	struct string_table_entry *next; /* next item in the link list/chain */
 };
 
 /*
@@ -20,15 +19,13 @@ struct string_table_entry {
  * seperate chain strategy for collisions
  */
 struct string_table {
-	size_t count;
-	size_t capacity;
-	struct string_table_entry **entries;
+	size_t count; /* number of strings in the string table */
+	size_t capacity; /* capacity of the string table before resizing it */
+	struct string_table_entry **entries; /* entries in the string table */
 };
 
 void string_table_init(struct string_table *table);
-
 void string_table_deinit(struct string_table *table);
-
 struct string_object *string_table_intern(struct string_table *table, char *val, size_t len);
 
 #endif /* STRING_H */
