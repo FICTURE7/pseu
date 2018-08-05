caddd0343cba786ac7c0026eeb36db2270233e8f
FICTURE7 <ficture7@gmail.com>
2018-08-05 17:49:27 +0400

    pretty: clean up

--- lib/pretty.h
+++ lib/pretty.h
@@ -1,11 +1,10 @@
 #ifndef PRETTY_H
 #define PRETTY_H
 
-#include "pretty.h"
 #include "node.h"
+#include "pretty.h"
 
 void prettyprint_token(struct token *token);
-
 void prettyprint_node(struct node *node);
 
-#endif
+#endif /* PRETTY_H */
